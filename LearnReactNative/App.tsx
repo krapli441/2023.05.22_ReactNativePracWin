d20638cd268d52c0a7727d310a0a4e6e02e53ff7
krapli441 <tlfqj1208@gmail.com>
2023-05-23 21:06:29 +0900

    안드로이드 구동 성공

--- LearnReactNative/App.tsx
+++ LearnReactNative/App.tsx
@@ -76,20 +76,10 @@ function App(): JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Hello Worlds!">
+            나는 <Text style={styles.highlight}>안드로이드</Text>에서 다시 한번
+            리액트 네이티브 앱을 구동해봤다.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
